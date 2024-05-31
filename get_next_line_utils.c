6bd61d7ff7f728af3cfff9dc61db343f2dd5c299
Catalina <152425031+cvicol1@users.noreply.github.com>
2024-05-31 18:03:43 +0200

    Add files via upload

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cvicol <cvicol@student.42madrid.com>       +#+  +:+       +#+        */
+/*   By: cvicol <cvicol@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:54:04 by cvicol            #+#    #+#             */
-/*   Updated: 2024/05/30 04:35:41 by cvicol           ###   ########.fr       */
+/*   Updated: 2024/05/31 17:32:38 by cvicol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
@@ -45,7 +35,9 @@ char	*ft_strdup(const char *s)
 	char	*s1;
 	int		i;
 
-	i = ft_strlen(s);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
 	s1 = malloc(sizeof(char) * (i + 1));
 	if (!s1)
 		return (NULL);
@@ -83,14 +75,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		lens1;
 	int		lens2;
 
+	lens1 = 0;
+	lens2 = 0;
 	if (!s1 && !s2)
 		return (NULL);
 	if (!s1)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	lens1 = ft_strlen(s1);
-	lens2 = ft_strlen(s2);
+	while (s1[lens1] != '\0')
+		lens1++;
+	while (s2[lens2] != '\0')
+		lens2++;
 	str = malloc (sizeof(char) * (lens1 + lens2 + 1));
 	if (!str)
 		return (NULL);
@@ -98,4 +94,28 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_memmove(str + lens1, s2, lens2);
 	str[lens1 + lens2] = '\0';
 	return (str);
+}
+
+char	*read_from_file(int fd)
+{
+	char	*buff;
+	int		n;
+
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	n = read(fd, buff, BUFFER_SIZE);
+	if (n <= 0)
+	{
+		free(buff);
+		buff = NULL;
+		return (NULL);
+	}
+	if (read(fd, 0, 0) < 0)
+	{
+		free (buff);
+		buff = NULL;
+	}
+	buff[n] = '\0';
+	return (buff);
 }
